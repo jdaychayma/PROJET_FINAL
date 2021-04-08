be1cc15481f62bbd0e74bfaad7bd4882600f3eab
jdaychayma <77275724+jdaychayma@users.noreply.github.com>
2021-04-09 00:36:32 +0100

    last commit

--- PROJET_FINAL/client/src/js/reducers/index.js
+++ PROJET_FINAL/client/src/js/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
-import PortfolioReducer from './PortfolioReducer';
 import ProductReducer from './ProductReducer';
 import category from './category';
 import panierReducer from './PanierReducer';
-export default combineReducers({ authReducer,PortfolioReducer,ProductReducer, category,panierReducer});
+export default combineReducers({ authReducer,ProductReducer, category,panierReducer});
