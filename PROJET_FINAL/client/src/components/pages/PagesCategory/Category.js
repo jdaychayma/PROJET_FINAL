5689dc9cdf545e599a7d4feea583931b278a4336
jdaychayma <77275724+jdaychayma@users.noreply.github.com>
2021-04-09 00:20:13 +0100

    Update Category.js

--- PROJET_FINAL/client/src/components/pages/PagesCategory/Category.js
+++ PROJET_FINAL/client/src/components/pages/PagesCategory/Category.js
@@ -14,10 +14,16 @@ const dispatch = useDispatch();
     const myCategory = category && category.find(el=>el._id === match.params.id)
   return (
 <div>
-    <div style={{position:"relative",textAlign:"center",color:"white"}}>
-        <img src={myCategory.imgUrl} alt='cat' width='100%' height='300'/>
+    <div style={{position:"relative",textAlign:"center",color:"black" , margin : "20px" , borderReduce : "30px"}}>
+   
+        <img style ={{borderRadius : "30px"}} src={myCategory.imgUrl} alt='cat' width='100%' height='300'/>
+        <br/>
+        <br/>
+        <h5 style={{position:"relative",textAlign:"center",color:"red"}}>{myCategory.description}</h5>
     <h1 style={{position:"absolute",top:'50%',right:'50%'}}>{myCategory&& myCategory.name}</h1>
+    
     </div>
+   
 
  <div className='container' style={{display:"flex",flexWrap:"wrap", flexDirection:"row"}}>
      {
